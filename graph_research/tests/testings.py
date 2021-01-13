74f597e8845d668b0abc7a9bfc2d2cc282eff376
Pol92 <plb92@hotmail.com>
2021-01-13 14:03:33 +0100

    Testings

--- graph_research/tests/testings.py
+++ graph_research/tests/testings.py
@@ -9,4 +9,8 @@ pol1 = sympy.poly(332.0*p**15 - 1524.0*p**14 + 2640.0*p**13 - 2047.0*p**12 + 600
 #Poly(-6.0*p**6 + 24.0*p**5 - 33.0*p**4 + 16.0*p**3, p, domain='RR')
 pol2 = sympy.poly(-6.0*p**6 + 24.0*p**5 - 33.0*p**4 + 16.0*p**3)
 
-print(Utilities.polynomial2binomial(pol2))
+
+#Poly(31.0*p**9 - 153.0*p**8 + 288.0*p**7 - 246.0*p**6 + 81.0*p**5, p, domain='RR')
+pol3 = sympy.poly(31.0*p**9 - 153.0*p**8 + 288.0*p**7 - 246.0*p**6 + 81.0*p**5)
+
+print(Utilities.polynomial2binomial(pol3))
