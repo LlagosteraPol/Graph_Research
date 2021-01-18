88f2f9e6cf891c8bb566fdda9ab773fe33b8fee2
Pol92 <plb92@hotmial.com>
2021-01-18 20:39:59 +0100

    Added a small file to do raw testings

--- graph_research/tests/testings.py
+++ graph_research/tests/testings.py
@@ -13,4 +13,7 @@ pol2 = sympy.poly(-6.0*p**6 + 24.0*p**5 - 33.0*p**4 + 16.0*p**3)
 #Poly(31.0*p**9 - 153.0*p**8 + 288.0*p**7 - 246.0*p**6 + 81.0*p**5, p, domain='RR')
 pol3 = sympy.poly(31.0*p**9 - 153.0*p**8 + 288.0*p**7 - 246.0*p**6 + 81.0*p**5)
 
-print(Utilities.polynomial2binomial(pol3))
+#Poly(-2384.0*p**20 + 13504.0*p**19 - 30704.0*p**18 + 35040.0*p**17 - 20079.0*p**16 + 4624.0*p**15, p, domain='RR')
+pol4 = sympy.poly(-2384.0*p**20 + 13504.0*p**19 - 30704.0*p**18 + 35040.0*p**17 - 20079.0*p**16 + 4624.0*p**15)
+
+print(Utilities.polynomial2binomial(pol4))
