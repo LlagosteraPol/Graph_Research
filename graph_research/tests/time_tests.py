ffea44637a8d89ff03bb231ed31356b166aacdab
Pol Llagostera Blasco <plb92@hotmail.com>
2021-07-02 11:01:40 +0200

    Improved, cleaned and optimized code to check the computational time of the Contraction-Deletion basic vs advanced algorithms

--- graph_research/tests/time_tests.py
+++ graph_research/tests/time_tests.py
@@ -2,110 +2,64 @@ from graph_research.core.utilities import *
 from graph_research.core.graphtbox import *
 import networkx as nx
 import matplotlib.pyplot as plt
+from enum import Enum
 
+class GType(Enum):
+    Hypercube = 1
+    Grid = 2
+    CircularLadder = 3
+    Complete = 4
 
-print("----------------------------------------------Hypercube------------------------------------------------")
+def compare_times(gtype, n_max):
+    print("----------------------------------------------", gtype.name, "------------------------------------------------")
+    times = dict()
+    for n in range(2, n_max + 1):
+        print("\n-----------> n =", n)
+        if gtype == GType.Hypercube:
+            g = nx.hypercube_graph(n)
+        elif gtype == GType.Grid:
+            g = nx.grid_graph([n,n])
+        elif gtype == GType.CircularLadder:
+            g = nx.circular_ladder_graph(n)
+        elif gtype == GType.Complete:
+            g = nx.complete_graph(n)
 
-for n in range(2, 4+1):
-    hg = nx.hypercube_graph(n)
+        start1 = time.time()
+        poly = GraphRel.relpoly_binary_basic(g)
+        end1 = time.time()
+        t1 = end1 - start1
+        print(Utilities.polynomial2binomial(poly))
+        print("Basic - ", gtype.name, " ", n, ":", t1)
 
-    start1 = time.time()
-    poly = GraphRel.relpoly_binary_basic(hg)
-    end1 = time.time()
-    print(Utilities.polynomial2binomial(poly))
-    print("Basic - Hypercube ", n, ":", end1 - start1)
+        start2 = time.time()
+        poly = GraphRel.relpoly_binary_improved(g)
+        end2 = time.time()
+        t2 = end2 - start2
+        print(Utilities.polynomial2binomial(poly))
+        print("Advanced - ", gtype.name," ", n, ":", t2)
 
-    cycles_edges, bal_pedges = GraphTools.get_cycles_edges(hg)
+        times[n] = (t1, t2)
+        try:
+            print("SP efficientcy compared to basic: ", round(t1 * 100 / t2, 3), "%")
 
-    start2 = time.time()
-    poly = GraphRel.relpoly_treecyc(hg, cycles_edges)
-    end2 = time.time()
-    print(Utilities.polynomial2binomial(poly))
-    print("Advanced - Hypercube ", n, ":", end2 - start2)
+        except (ZeroDivisionError):
+            print("SP efficientcy compared to basic: ", round(t1 * 100, 3), "%")
+    print("-------------------------------------------------------------------------------------------------------")
+    return times
 
-    try:
-        print("SP efficientcy compared to basic: ", round((end1-start1)*100/(end2-start2), 3), "%")
+def ctime_hypercube(n_max):
+    compare_times(GType.Hypercube, n_max)
 
-    except (ZeroDivisionError):
-        print("SP efficientcy compared to basic: ", round((end1 - start1) * 100, 3), "%")
-print("-------------------------------------------------------------------------------------------------------")
+def ctime_grid(n_max):
+    compare_times(GType.Grid, n_max)
 
-print("----------------------------------------------Grid------------------------------------------------")
+def ctime_circularladder(n_max):
+    compare_times(GType.CircularLadder, n_max)
 
-for n in range(2, 10+1):
-    gg = nx.grid_graph(n, n)
+def ctime_complete(n_max):
+    compare_times(GType.Complete, n_max)
 
-    start1 = time.time()
-    poly = GraphRel.relpoly_binary_basic(gg)
-    end1 = time.time()
-    print(Utilities.polynomial2binomial(poly))
-    print("Basic - Grid ", n, ":", end1 - start1)
-
-    cycles_edges, bal_pedges = GraphTools.get_cycles_edges(gg)
-
-    start2 = time.time()
-    poly = GraphRel.relpoly_treecyc(gg, cycles_edges)
-    end2 = time.time()
-    print(Utilities.polynomial2binomial(poly))
-    print("Advanced - Grid ", n, ":",  end2 - start2)
-
-    try:
-        print("SP efficientcy compared to basic: ", round((end1-start1)*100/(end2-start2), 3), "%")
-
-    except (ZeroDivisionError):
-        print("SP efficientcy compared to basic: ", round((end1 - start1) * 100, 3), "%")
-print("-------------------------------------------------------------------------------------------------------")
-
-
-print("-------------------------------------------Circular Ladder---------------------------------------------")
-
-for n in range(2, 10+1):
-    gg = nx.circular_ladder_graph(n)
-
-    start1 = time.time()
-    poly = GraphRel.relpoly_binary_basic(gg)
-    end1 = time.time()
-    print(Utilities.polynomial2binomial(poly))
-    print("Basic - Grid ", n, ":", end1 - start1)
-
-    cycles_edges, bal_pedges = GraphTools.get_cycles_edges(gg)
-
-    start2 = time.time()
-    poly = GraphRel.relpoly_treecyc(gg, cycles_edges)
-    end2 = time.time()
-    print(Utilities.polynomial2binomial(poly))
-    print("Advanced - Grid ", n, ":",  end2 - start2)
-
-    try:
-        print("SP efficientcy compared to basic: ", round((end1-start1)*100/(end2-start2), 3), "%")
-
-    except (ZeroDivisionError):
-        print("SP efficientcy compared to basic: ", round((end1 - start1) * 100, 3), "%")
-print("-------------------------------------------------------------------------------------------------------")
-
-
-print("----------------------------------------------Complete------------------------------------------------")
-
-for n in range(2, 10+1):
-    gg = nx.grid_graph(n, n)
-
-    start1 = time.time()
-    poly = GraphRel.relpoly_binary_basic(gg)
-    end1 = time.time()
-    print(Utilities.polynomial2binomial(poly))
-    print("Basic - Complete ", n, ":", end1 - start1)
-
-    cycles_edges, bal_pedges = GraphTools.get_cycles_edges(gg)
-
-    start2 = time.time()
-    poly = GraphRel.relpoly_treecyc(gg, cycles_edges)
-    end2 = time.time()
-    print(Utilities.polynomial2binomial(poly))
-    print("Advanced - Complete ", n, ":",  end2 - start2)
-
-    try:
-        print("SP efficientcy compared to basic: ", round((end1-start1)*100/(end2-start2), 3), "%")
-
-    except (ZeroDivisionError):
-        print("SP efficientcy compared to basic: ", round((end1 - start1) * 100, 3), "%")
-print("-------------------------------------------------------------------------------------------------------")
+#ctime_hypercube(3)
+#ctime_grid(3)
+#ctime_circularladder(3)
+ctime_complete(3)
